3ebb6e9714dde3c9a197f97a691f382c896f81d4
Sumedh-smd <90448315+Sumedh-smd@users.noreply.github.com>
2022-01-07 15:37:26 +0530

    main project

---
+++ replyer.py
@@ -0,0 +1,37 @@
+from tkinter import *
+
+window = Tk()
+
+window.geometry('300x100')
+
+replyes = {'how are you': "I'm Good !!!", 'hello': 'Heyy !!!', 'hi': 'Heyy'}
+
+# while True:
+#     ask = input(' \t > ')
+#     if 'Bye' in ask or 'bye' in ask:
+#         print(spl('Bye !!!'))
+#         break
+#     else:
+#         call(ask)
+
+user_rep = Entry(window,width=25)
+user_rep.grid(column=0, row=0)
+
+com_rep = Label(window,text='Reply...')
+com_rep.grid(column=0, row=2)
+
+
+def clicked():
+    inp = user_rep.get()
+    new_inp = inp[0].lower() + inp[1:]
+    if new_inp in replyes.keys():
+        com_rep.configure(text=f'{replyes[new_inp]}')
+    else:
+        com_rep.configure(text='Hmmm')
+
+user_btn = Button(window, text='Submit', bg='white',fg='blue',command=clicked)
+user_btn.grid(column=1,row=0)
+
+
+
+window.mainloop()
